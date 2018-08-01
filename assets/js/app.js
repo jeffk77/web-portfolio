f212ef96d1f6d917bf2afef15620c016e39d7226
jeffk77 <jeff.kushner@me.com>
2018-07-31 20:46:47 -0400

    Major Layout Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
+
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, options);
+
 });
 
 // Or with jQuery
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('.sidenav').sidenav();
+  $('.carousel').carousel();
+  $('.parallax').parallax();
 });
